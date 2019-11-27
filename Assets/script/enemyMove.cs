942be1cc4a8e9af2fda844d608597883a3ecd4e9
fiqih021 <fiqihhamdareza021@gmail.com>
2019-11-28 05:26:25 +0800

    +enemy

---
+++ Assets/script/enemyMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMove : MonoBehaviour {
+
+	private Vector3 naik;
+	private Vector3 turun;
+	private Vector3 nexPos;
+	Player komponenGerak;
+
+	[SerializeField] private float speed = 50;
+	[SerializeField] private Transform mace;
+	[SerializeField] private Transform transformB;
+	// Use this for initialization
+	void Start () {
+		naik = mace.localPosition;
+		turun = transformB.localPosition;
+		nexPos = turun;
+		komponenGerak = GameObject.Find("Player").GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	private void Move(){
+		mace.localPosition = Vector3.MoveTowards(mace.localPosition,nexPos,speed * Time.deltaTime);
+
+		if (Vector3.Distance(mace.localPosition,nexPos) <= .1f)
+		{
+			tukar();
+		}
+	}
+
+	private void tukar(){
+		if (nexPos != naik)
+		{
+			nexPos = naik;
+			speed = 10;
+		}
+		else
+		{
+			nexPos = turun;
+			speed = 50;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player")
+		{
+			Destroy(komponenGerak.gameObject);
+		}
+	}
+}
